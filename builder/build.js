35cf4bcd3524f4cd5fb1e462f420274d4d4edcc7
Ger Hobbelt <ger@hobbelt.com>
2013-02-12 05:52:31 +0100

    fix build script: we're not in SVN (googlecode) anymore!

--- builder/build.js
+++ builder/build.js
@@ -294,7 +294,7 @@ function zip() {
 var actions = [
     deleteBuildDir,
     createBuildDir,
-    checkoutSvnRepository,
+    /* checkoutSvnRepository, */
     getVersion,
     concatCoreScripts,
     copyModuleScripts,
